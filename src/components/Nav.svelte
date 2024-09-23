00417bc4467aa7def4bd951eeea7349042907ff6
Matias Gutierrez <mgutierrez@brainfood.cl>
2024-09-23 14:20:06 -0300

    Added simple Navbar

---
+++ src/components/Nav.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<nav class="bg-white shadow-md">
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<div class="flex h-16 items-center justify-between">
+			<div class="flex-shrink-0">
+				<img class=" h-16 w-auto" src="/bf_logo2.png" alt="Logo de BrainFood" />
+			</div>
+			<div class="flex flex-1 justify-center">
+				<span class="text-xl font-semibold text-gray-900">Brainfood Project Library</span>
+			</div>
+			<!-- Este div vacío es para mantener el nombre centrado -->
+			<div class="w-8 flex-shrink-0" />
+		</div>
+	</div>
+</nav>
